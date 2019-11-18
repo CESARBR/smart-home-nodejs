f3005636109748500ca5f6e80d07811473e83179
Claudio Takahasi <ckt@cesar.org.br>
2019-11-18 14:32:56 -0300

    Fix REQUEST message parsing
    This patch fixes the json (dictionary) parsing of the REQUEST message.
    Device id is not being mapped properly. This is a testing purpose client.
    On/Off to any device will switch on/off the physical LED.

--- rpi3.py
+++ rpi3.py
@@ -1,34 +1,53 @@
+#
+# Testing purpose only. This python script is socketio
+# client that receives a request (EXECUTE intent) and
+# perform an action in the physical device.
+#
+
 import socketio
 import RPi.GPIO as GPIO
 import logging
 import json
 
-
+logging.basicConfig(level=logging.DEBUG)
 sio = socketio.Client()
-# initial GPIO setup
+#initial GPIO setup
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT, initial=GPIO.LOW)
 
-
 @sio.event
 def connect():
-    print('socket.io established')
+    print('Connection Established!')
 
 @sio.event
 def message(data):
-    print('Message:', data)
+    print('Message received with ', data)
 
 @sio.on('REQUEST')
-def onrequest(param):
-    print('Assistant request:', param)
+def on_json(data):
+    print('Received Assistant request: ', data)
     sio.emit('RESPONSE', {"status": 200})
 
-    if (param["params"]["on"] == True):
-        logging.info('Turning led on')
+    # identify the device
+    if (data["id"] == '1emw'):
+        logging.info('Table Lamp')
+    elif (data["id"] == 'p5e'):
+        logging.info('Daddy Lamp')
+    elif (data["id"] == '1ris'):
+        logging.info('Ceilling lamp')
+    elif (data["id"] == 'spf'):
+        logging.info('Thermostat')
+
+    # GPIO action
+    # Workaround: Turn on/off is not mapped
+    # to a specific device. On/Off request
+    # to any device will change the LED status.
+    if (data["params"]["on"] == True):
+        logging.info('Turning LED on')
         GPIO.output(17,1)
-    elif (param["params"]["on"]  == False):
-        logging.info('Turning led off')
+    elif (data["params"]["on"]  == False):
+        logging.info('Turning LED off')
         GPIO.output(17,0)
 
 @sio.event
